df855c8750567b01d5ae4abe19597441c6f0a3b0
⚡ ⚡ <galbujar@mail.valenciacollege.edu>
2022-11-10 21:33:08 -0500

    final

--- main.js
+++ main.js
@@ -2,17 +2,13 @@ for (let i = 1000; i >= 0; i -= 2) {
 console.log(i);
 }
 
-for (let i = 0; i < 10000; i++) {    
-}
-if (i = 2500){
-    alert("A quarter of the way there!"); 
-} else if (i = 5000){
-    alert("Halfway there!");
-} else{ 
-  alert("The loop is done!")
+for (let i = 0; i <= 10000; i++){ 
+    if (i === 2500) alert("A quarter of the way there!"); 
+    else if (i === 5000) alert("Halfway there!");
+    else if (i === 10000) alert("The loop is done!");
 }
 
-const tvShows = ["That 70's Show","Money Heist", "Stranger Things", "The Office", "Friends"];
-
-console.log("My # " + "favorite TV show is " + tvShows[0, 1, 2, 3, 4]);
-
+const tvShows = ["Money Heist", "Stranger Things", "Black Mirror", "Euphoria", "That 70's Show"];
+for (let i = 0; i < tvShows.length; i++) {
+    console.log("My #" + [i + 1] + " favorite TV show is " + tvShows[i]);
+}
