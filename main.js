ca1518d48ba878d8156694f217b35da50a8b4451
⚡ ⚡ <galbujar@mail.valenciacollege.edu>
2022-11-10 21:34:55 -0500

    finished

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ console.log(i);
 }
 
 for (let i = 0; i <= 10000; i++){ 
-    if (i === 2500) alert("A quarter of the way there!"); 
-    else if (i === 5000) alert("Halfway there!");
-    else if (i === 10000) alert("The loop is done!");
+    if (i === 2500) 
+    alert("A quarter of the way there!"); 
+    else if (i === 5000) 
+    alert("Halfway there!");
+    else if (i === 10000) 
+    alert("The loop is done!");
 }
 
 const tvShows = ["Money Heist", "Stranger Things", "Black Mirror", "Euphoria", "That 70's Show"];
